e265af6c0548ff576cd6166d0b4564e05ee10bcd
hallb2 <43815701+hallb2@users.noreply.github.com>
2019-04-28 22:24:28 -0400

    Add files via upload

---
+++ ESP01.sh
@@ -0,0 +1,12 @@
+adapter_location="$(dmesg | grep uart | grep attached | tail -1 | awk 'NF{print $NF; exit}')"
+
+echo $adapter_location
+
+sudo esptool.py --port /dev/$adapter_location erase_flash
+
+echo -e "Short the GND and RST pins on the adapter. Press enter to continue."
+read enter
+
+sudo esptool.py --port /dev/$adapter_location --baud 115200 write_flash --flash_size=detect 0 /home/$USER/Downloads/esp8266-20190125-v1.10.bin
+
+echo "It worked"
